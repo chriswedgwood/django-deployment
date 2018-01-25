8f494033929676288041b4810e99d2bb89ccecae
chriswedgwood <wedgemail@gmail.com>
2018-01-25 15:54:26 +0000

    create_new_user

--- create_new_user.sh
+++ create_new_user.sh
@@ -3,31 +3,26 @@
 #wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/chriswedgwood/django-deployment/master/create_new_user.sh
 
 read -p "Application User:" APPLICATION_USER
+read -p "Server IP:" IPADDRESS
 
-
-
-sudo -u postgres psql -f create.sql
-rm create.sql
-
+BLUE='\033[0;34m'
 
 adduser $APPLICATION_USER
 usermod -aG sudo $APPLICATION_USER
 
-echo "RUN THESE NEXT...."
-echo "mkdir .ssh"
-echo "touch .ssh/authorized_keys"
-echo "chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
-
-
+echo -e "${BLUE}RUN THESE NEXT...."
+echo -e "${BLUE}mkdir .ssh"
+echo -e "${BLUE}touch .ssh/authorized_keys"
+echo -e "${BLUE}chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
 
 su - $APPLICATION_USER
 
-echo "#################################################################################"
-echo "run exit"
-echo "NOW RUN cat ~/.ssh/id_rsa.pub locally on your laptop"
-echo "THEN LOGIN with root - ssh root@ip.ad.dr.es"
-echo "su - $APPLICATION_USER"
-echo "emacs .ssh/authorized_keys"
-echo "PASTE CONTENTS OF CLIPBOARD INTO FILE"
-echo "CTR-x CTR-s CTR-z"
-echo "TRY ssh with $APPLICATION_USER - ssh $APPLICATION_USER@ip.ad.dr.es"
+echo -e "${BLUE}#################################################################################"
+echo -e "${BLUE}run exit"
+echo -e "${BLUE}NOW RUN cat ~/.ssh/id_rsa.pub locally on your laptop"
+echo -e "${BLUE}THEN LOGIN with root - ssh root@$IPADDRESS"
+echo -e "${BLUE}su - $APPLICATION_USER"
+echo -e "${BLUE}emacs .ssh/authorized_keys"
+echo -e "${BLUE}PASTE CONTENTS OF CLIPBOARD INTO FILE"
+echo -e "${BLUE}CTR-x CTR-s CTR-z"
+echo -e "${BLUE}TRY ssh with $APPLICATION_USER - ssh $APPLICATION_USER@$IPADDRESS"
--- initialise.sh
+++ initialise.sh
@@ -44,25 +44,6 @@ sudo -u postgres psql -f create.sql
 rm create.sql
 
 
-adduser $APPLICATION_USER
-usermod -aG sudo $APPLICATION_USER
-
-echo -e "${BLUE}RUN THESE NEXT...."
-echo -e "${BLUE}mkdir .ssh"
-echo -e "${BLUE}touch .ssh/authorized_keys"
-echo -e "${BLUE}chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
-
-su - $APPLICATION_USER
-
-echo -e "${BLUE}#################################################################################"
-echo -e "${BLUE}run exit"
-echo -e "${BLUE}NOW RUN cat ~/.ssh/id_rsa.pub locally on your laptop"
-echo -e "${BLUE}THEN LOGIN with root - ssh root@$IPADDRESS"
-echo -e "${BLUE}su - $APPLICATION_USER"
-echo -e "${BLUE}emacs .ssh/authorized_keys"
-echo -e "${BLUE}PASTE CONTENTS OF CLIPBOARD INTO FILE"
-echo -e "${BLUE}CTR-x CTR-s CTR-z"
-echo -e "${BLUE}TRY ssh with $APPLICATION_USER - ssh $APPLICATION_USER@$IPADDRESS"
 
 
 
