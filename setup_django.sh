e59629bf64f19c1dfdb9ed4c474e097cd8e3effc
chriswedgwood <wedgemail@gmail.com>
2018-01-26 14:32:55 +0000

    gunicorn

--- setup_django.sh
+++ setup_django.sh
@@ -3,6 +3,19 @@
 
 #locales http://smekalov.me/ubuntu-and-locales/
 
-virtualenv .
-source bin/activate
+#virtualenv .
+#source bin/activate
 
+read -p "Server IP:" IPADDRESS
+
+echo "export DJANGO_SETTINGS_MODULE='config.settings.production'
+export DJANGO_SECRET_KEY=''
+export DJANGO_ALLOWED_HOSTS='$IPADDRESS'
+export DJANGO_ADMIN_URL='ysadmin'
+export DJANGO_MAILGUN_API_KEY=''
+export DJANGO_MAILGUN_SERVER_NAME=''
+export DJANGO_MAILGUN_SENDER_DOMAIN=''
+export DJANGO_AWS_ACCESS_KEY_ID=''
+export DJANGO_AWS_SECRET_ACCESS_KEY=''
+export DJANGO_AWS_STORAGE_BUCKET_NAME=''
+export DATABASE_URL='postgres://youstayuk:Tw@ngers@pythonwedge-449.postgres.pythonanywhere-services.com:10449/youstayuk'"
