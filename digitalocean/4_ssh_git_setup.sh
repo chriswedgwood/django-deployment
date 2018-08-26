1d2102ffb3f988d09ed377f99da3861b5af611af
Chris wedgwood <chriswedgwood@Chriss-MBP.home>
2018-08-26 08:59:37 +0100

    ssh

--- digitalocean/4_ssh_git_setup.sh
+++ digitalocean/4_ssh_git_setup.sh
@@ -7,8 +7,8 @@ source .env
 echo -e "${CYAN}####SETUP SSH FOR GITHUB CLONES####${NC}"
 ssh-keygen -t rsa -b 4096 -C "wedgemail@gmail.com"
 echo -e "${CYAN}####CHECK ssh-agent IS UP####${NC}"
-eval $(ssh-agent -s)
 echo -e "${CYAN}####adding id_rsa to agent. You need to re-enter the ssh key passphrase ####${NC}"
+eval $(ssh-agent -s)
 ssh-add /home/$APPLICATION/.ssh/id_rsa
 echo -e "${CYAN}####COPY THIS KEY TO GITHUB####${NC}"
-cat cat /home/$APPLICATION/.ssh/id_rsa.pub
+cat /home/$APPLICATION/.ssh/id_rsa.pub
